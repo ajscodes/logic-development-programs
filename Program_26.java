f1eaafc6bc0c10cf57fd88eaa35245e828fd2782
ajscodes <ayushmaradia@gmail.com>
2025-11-11 23:14:04 +0530

    26 Done

--- Program_26.java
+++ Program_26.java
@@ -6,38 +6,59 @@ public class Program_26 {
         System.out.print("Enter size of array: ");
         int n = sc.nextInt();
 
+        if (n < 2) {
+            System.out.println("Array must contain at least two elements.");
+            return;
+        }
+
         int[] arr = new int[n];
-        System.out.println("Enter array elements:");
+
         for (int i = 0; i < n; i++) {
+            System.out.print("Enter " + i + "th element: ");
             arr[i] = sc.nextInt();
         }
 
-        int largest = Integer.MIN_VALUE;
-        int secondLargest = Integer.MIN_VALUE;
-        int smallest = Integer.MAX_VALUE;
-        int secondSmallest = Integer.MAX_VALUE;
+        int max1, max2, min1, min2;
 
-        for (int i = 0; i < n; i++) {
-            if (arr[i] > largest) {
-                secondLargest = largest;
-                largest = arr[i];
-            } else if (arr[i] > secondLargest && arr[i] != largest) {
-                secondLargest = arr[i];
-            }
+        if (arr[0] > arr[1]) {
+            max1 = arr[0];
+            max2 = arr[1];
+            min1 = arr[1];
+            min2 = arr[0];
+        } else if (arr[0] < arr[1]) {
+            max1 = arr[1];
+            max2 = arr[0];
+            min1 = arr[0];
+            min2 = arr[1];
+        } else {
+            max1 = max2 = arr[0];
+            min1 = min2 = arr[0];
+        }
 
-            if (arr[i] < smallest) {
-                secondSmallest = smallest;
-                smallest = arr[i];
-            } else if (arr[i] < secondSmallest && arr[i] != smallest) {
-                secondSmallest = arr[i];
+        for (int i = 2; i < n; i++) {
+            if (arr[i] > max1) {
+                max2 = max1;
+                max1 = arr[i];
+            } else if (arr[i] < max1 && (max2 == max1 || arr[i] > max2)) {
+                max2 = arr[i];
             }
         }
 
-        System.out.println("\nLargest element: " + largest);
-        System.out.println("Second largest element: " + secondLargest);
-        System.out.println("Smallest element: " + smallest);
-        System.out.println("Second smallest element: " + secondSmallest);
-        System.out.println("Difference: "+(secondLargest - secondSmallest));
+        for (int i = 2; i < n; i++) {
+            if (arr[i] < min1) {
+                min2 = min1;
+                min1 = arr[i];
+            } else if (arr[i] > min1 && (min2 == min1 || arr[i] < min2)) {
+                min2 = arr[i];
+            }
+        }
+
+        System.out.println();
+        System.out.println("Max = " + max1);
+        System.out.println("Second Max = " + max2);
+        System.out.println("Min = " + min1);
+        System.out.println("Second Min = " + min2);
+        System.out.println("Difference = " + (max2 - min2));
 
         sc.close();
     }
