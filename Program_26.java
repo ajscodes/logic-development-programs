1c63aff30ce42f9f79d251227e6145a0bf1746e6
ajscodes <ayushmaradia@gmail.com>
2025-11-11 13:35:39 +0530

    Self pending

---
+++ Program_26.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Program_26 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+        int smallest = Integer.MAX_VALUE;
+        int secondSmallest = Integer.MAX_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > largest) {
+                secondLargest = largest;
+                largest = arr[i];
+            } else if (arr[i] > secondLargest && arr[i] != largest) {
+                secondLargest = arr[i];
+            }
+
+            if (arr[i] < smallest) {
+                secondSmallest = smallest;
+                smallest = arr[i];
+            } else if (arr[i] < secondSmallest && arr[i] != smallest) {
+                secondSmallest = arr[i];
+            }
+        }
+
+        System.out.println("\nLargest element: " + largest);
+        System.out.println("Second largest element: " + secondLargest);
+        System.out.println("Smallest element: " + smallest);
+        System.out.println("Second smallest element: " + secondSmallest);
+        System.out.println("Difference: "+(secondLargest - secondSmallest));
+
+        sc.close();
+    }
+}
