87d3ea8e46897cf1a8829a887b1b5d5c1cf99562
ajscodes <ayushmaradia@gmail.com>
2025-11-13 12:40:03 +0530

    PDF

---
+++ Program_28.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Program_28 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter string: ");
+        String s = sc.nextLine();
+
+        int i = 0;
+        String result;
+        
+        while(i < s.length()){
+            char ch = s.charAt(i);
+            if(ch >= '1' && ch <= '9'){
+
+            }
+        }
+
+        sc.close();
+    }
+}
